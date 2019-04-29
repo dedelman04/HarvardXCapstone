0f5f3becdc8e18df4bdd6329df2a827edaabd333
David Edelman <dedelman@cameronlng.com>
2019-04-29 15:12:24 -0500

    more rmd and cleaning up report.r

--- report.R
+++ report.R
@@ -2,6 +2,8 @@ if(!require(lubridate)) install.packages("lubridate", repos = "http://cran.us.r-
 if(!require(data.table)) install.packages("data.table", repos = "http://cran.us.r-project.org")
 if(!require(tidyverse)) install.packages("tidyverse", repos = "http://cran.us.r-project.org")
 if(!require(caret)) install.packages("caret", repos = "http://cran.us.r-project.org")
+if(!require(gridExtra)) install.packages("gridExtra", repos = "http://cran.us.r-project.org")
+if(!require(qcc)) install.packages("qcc", repos = "http://cran.us.r-project.org")
 
 #To reset if something messes up
 #edx_base <- edx
@@ -19,92 +21,6 @@ edx %>% mutate(gp_rating = floor(rating)) %>%
 
 #Mean rating
 mu <- mean(edx$rating)
-
-####
-#movie_yr, review_yr --> weighted "nostalgia" factor
-#mean across all genres
-#ignore zero variance users?
-
-###Create "nostalgia index" - number of years between movie release and review
-
-#Add review year
-edx <- edx %>% mutate(review_yr = year(as.Date(((edx$timestamp/60)/60)/24, origin="1970-1-1")))
-
-#Extract movie year from title
-edx <- edx %>% separate(title, into=c("ttl", "movie_yr"), sep= -5, 
-                        remove=FALSE, fill="right", extra="merge")
-
-edx <- edx %>% mutate(movie_yr = as.numeric(sub(")", "", edx$movie_yr)))
-edx <- edx %>% select(-ttl)
-
-#Calculate years between movie release and review; negative numbers are assumed to be typos
-#and will be set to 0
-edx <- edx %>% mutate(nostalgia_idx = ifelse(review_yr < movie_yr, 0, review_yr - movie_yr))
-
-edx %>% ggplot(aes(x=nostalgia_idx))+geom_histogram(binwidth=1)+scale_y_log10()
-
-edx %>% group_by(nostalgia_idx) %>% summarize(mean_rt = mean(rating)) %>% 
-  ggplot(aes(x=nostalgia_idx, y=mean_rt))+geom_line()
-#    geom_vline(xintercept = 15)+
-#    geom_vline(xintercept = 30)+
-#    geom_vline(xintercept = 45)+
-#    geom_vline(xintercept = 60)+
-#    geom_vline(xintercept = 75)+
-#    geom_vline(xintercept = 60)+
-#    geom_vline(xintercept = 70)+
-
-p <- edx %>% group_by(movie_yr) %>% summarize(mean_rt = mean(rating)) %>%
-  ggplot(aes(x=movie_yr, y=mean_rt))+geom_line(color="red") #+scale_x_reverse()
-
-edx %>% group_by(movie_yr) %>% summarize(mean_rt = mean(rating)) %>%
-  cor()
-
-review <- edx %>% group_by(review_yr) %>% summarize(mean_rt = mean(rating))
-
-p <- p + 
-  geom_line(data = edx %>% group_by(review_yr) %>% summarize(mean_rt = mean(rating)),
-            mapping = aes(x=review_yr, y=mean_rt),
-            color="blue") #+scale_x_reverse()
-
-p <- p +
-  geom_line(data = edx %>% group_by(nostalgia_idx) %>%
-              summarize(mean_rt = mean(rating)),
-            mapping = aes(x=2010-nostalgia_idx, y=mean_rt),
-            color = "black")
-
-p + legend()
-
-edx %>% group_by(review_yr) %>% summarize(mean_rt = mean(rating)) %>%
-  cor()
-
-edx %>% group_by(nostalgia_idx) %>% summarize(mean_rt = mean(rating)) %>% 
-  cor()
-
-edx %>% filter(nostalgia_idx <= 55) %>% 
-  group_by(nostalgia_idx) %>% summarize(mean_rt = mean(rating)) %>%
-  cor()
-
-edx %>% filter(nostalgia_idx > 55) %>% 
-  group_by(nostalgia_idx) %>% summarize(mean_rt = mean(rating)) %>%
-  cor()
-
-#Pareto chart
-nost <- edx %>% group_by(nostalgia_idx) %>% summarize(n=n()) %>% arrange(nostalgia_idx) %>% .$n
-names(nost) <- distinct(data.frame(idx = edx$nostalgia_idx)) %>% arrange(idx) %>% .$idx
-pareto.chart(nost, ylab="total ratings", cumperc=seq(0,100, by=5))
-
-#Group nostalgia indices into nostalgia factors
-#facs <- c("0-10", "11-20", "21-30", "31-40", "41-50", "51-74", "75+")
-#cut_levels <- c(-0.5, 10.5, 20.5, 30.5, 40.5, 50.5, 74.5, 100)
-facs <- c("0-20", "21-35", "36-60", "61+")
-cut_levels <- c(-0.5, 20.5, 35.5, 60.5, 100)
-
-edx <- edx %>% mutate(nostalgia_factor = cut(nostalgia_idx, cut_levels, labels=facs))
-
-edx %>% group_by(nostalgia_factor) %>% summarize(mean_rt = mean(rating)) %>% 
-  ggplot(aes(x=nostalgia_factor, y=mean_rt))+geom_point()
-
-edx %>% ggplot(aes(x=rating))+geom_histogram(bins=5)+facet_wrap(~ nostalgia_factor, scales="free_y")
 
 ###Seperate genres
 #Find the longest genre entry
@@ -135,11 +51,61 @@ all_genres <- as.character(distinct(all_genres)$genre)
 
 edx <- edx %>% mutate_at(genres, ~as.character(.x))
 
-edx %>% ggplot(aes(x=rating))+geom_histogram(bins=5)+facet_wrap(~genre1, scales="free_y")
+edx %>% mutate(gp_rating = floor(rating)) %>%
+  ggplot(aes(x=gp_rating))+geom_histogram(stat="count")+facet_wrap(~genre1, scales="free_y")
 
-#edx <- edx %>% mutate_at(genres, ~ifelse(is.na(.x), "", .x))
 
-edx[edx$genre1=="IMAX",]
+###Create "nostalgia index" - number of years between movie release and review
+#Add review year
+edx <- edx %>% mutate(review_yr = year(as.Date(((edx$timestamp/60)/60)/24, origin="1970-1-1")))
+
+table(edx$review_yr) %>% as.data.frame() %>% arrange(desc(Freq))
+
+#Plot review year against mean rating, newer reviews first
+edx %>% filter(review_yr != 1995 & review_yr != 2009) %>%
+  group_by(review_yr) %>% summarize(mean_rt = mean(rating)) %>%
+  ggplot(aes(x=review_yr, y=mean_rt))+geom_line()+scale_x_reverse()
+
+    
+#Extract movie year from title
+edx <- edx %>% separate(title, into=c("ttl", "movie_yr"), sep= -5, 
+                        remove=FALSE, fill="right", extra="merge")
+
+edx <- edx %>% mutate(movie_yr = as.numeric(sub(")", "", edx$movie_yr)))
+edx <- edx %>% select(-ttl)
+
+range(edx$movie_yr)
+
+#Plot movie year against mean rating, newer movies first
+edx %>% group_by(movie_yr) %>% summarize(mean_rt = mean(rating)) %>%
+  ggplot(aes(x=movie_yr, y=mean_rt))+geom_line()+scale_x_reverse()+geom_smooth(linetype = 2, se=FALSE)
+
+#Calculate years between movie release and review; negative numbers are assumed to be typos
+#and will be set to 0
+edx <- edx %>% mutate(nostalgia_idx = ifelse(review_yr < movie_yr, 0, review_yr - movie_yr))
+
+edx %>% group_by(nostalgia_idx) %>% summarize(mean_rt = mean(rating)) %>% 
+  ggplot(aes(x=nostalgia_idx, y=mean_rt))+geom_line()
+
+
+#Pareto chart
+#nost <- edx %>% group_by(nostalgia_idx) %>% summarize(n=n()) %>% arrange(nostalgia_idx) %>% .$n
+#names(nost) <- distinct(data.frame(idx = edx$nostalgia_idx)) %>% arrange(idx) %>% .$idx
+#pareto.chart(nost, ylab="total ratings", cumperc=seq(0,100, by=5))
+
+#Group nostalgia indices into nostalgia factors
+#facs <- c("0-10", "11-20", "21-30", "31-40", "41-50", "51-74", "75+")
+#cut_levels <- c(-0.5, 10.5, 20.5, 30.5, 40.5, 50.5, 74.5, 100)
+facs <- c("0-20", "21-35", "36-60", "61+")
+cut_levels <- c(-0.5, 20.5, 35.5, 60.5, 100)
+
+edx <- edx %>% mutate(nostalgia_factor = cut(nostalgia_idx, cut_levels, labels=facs))
+
+edx %>% group_by(nostalgia_factor) %>% summarize(mean_rt = mean(rating)) %>% 
+  ggplot(aes(x=nostalgia_factor, y=mean_rt))+geom_point()
+
+edx %>% ggplot(aes(x=rating))+geom_histogram(bins=5)+facet_wrap(~ nostalgia_factor, scales="free_y")
+
 
 ###Look for users with little variance ( <= 1) in their ratings
 
